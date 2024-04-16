5ae532041ed4708d887bf3c46f2df682bb9fb5c7
William Muli <willi.wambu@gmail.com>
2024-04-16 10:23:08 +0300

    Make virtual dapp list full height

--- components/brave_wallet_ui/components/desktop/views/crypto/index.tsx
+++ components/brave_wallet_ui/components/desktop/views/crypto/index.tsx
@@ -353,6 +353,7 @@ export const CryptoView = ({ sessionRoute }: Props) => {
             cardHeader={
               <PageTitleHeader title={getLocale('braveWalletTopNavExplore')} />
             }
+            useFullHeight={true}
           >
             <StyledWrapper>
               {banners}

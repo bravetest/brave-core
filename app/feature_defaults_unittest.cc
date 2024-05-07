00bdf37cb7561616bb2da3f2d187d47eb2c8732c
Claudio DeSouza <cdesouza@brave.com>
2024-05-07 22:40:58 +0100

    Use `DisplayObserver::OnDisplaysRemoved`
    We override `DisplayObserver::OnDisplayRemoved` for a couple of things,
    although we make no use of the argument passed. This event is now only
    available supporting more than one display being removed. This is of no
    consequence for our uses.
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/ba3cd346f19fb60e8c116e18d385bbaf68eef9a3
    
    commit ba3cd346f19fb60e8c116e18d385bbaf68eef9a3
    Author: Vincent Chiang <vincentchiang@google.com>
    Date:   Mon May 6 22:09:36 2024 +0000
    
        Migrate OnDisplayRemoved to OnDisplaysRemoved
    
        Deprecate DisplayObserver::OnDisplayRemoved and use
        DisplayObserver::OnDisplaysRemoved instead.
    
        Bug: 329003664

--- app/feature_defaults_unittest.cc
+++ app/feature_defaults_unittest.cc
@@ -216,7 +216,6 @@ TEST(FeatureDefaultsTest, DisabledFeatures) {
 #endif
       &media::kLiveCaption,
       &metrics::structured::kEnabledStructuredMetricsService,
-      &metrics::structured::kNearbyShareMetrics,
       &metrics::structured::kPhoneHubStructuredMetrics,
       &net::features::kEnableWebTransportDraft07,
       &net::features::kTpcdMetadataGrants,

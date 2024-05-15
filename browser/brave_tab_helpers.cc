e8237ac39ba408b4ea6e604da6cae93ffbfa2e85
Vadym Struts <vstruts@brave.com>
2024-05-15 22:35:52 +0200

    removed ipfs interstitials, IpfsImportController, IPFSTabHelper, related tests
    Signed-off-by: Vadym Struts <vstruts@brave.com>

--- browser/brave_tab_helpers.cc
+++ browser/brave_tab_helpers.cc
@@ -82,11 +82,6 @@
 #include "brave/components/tor/tor_tab_helper.h"
 #endif
 
-#if BUILDFLAG(ENABLE_IPFS)
-#include "brave/browser/ipfs/ipfs_service_factory.h"
-#include "brave/browser/ipfs/ipfs_tab_helper.h"
-#endif
-
 #if BUILDFLAG(ENABLE_EXTENSIONS)
 #include "brave/browser/web_discovery/web_discovery_tab_helper.h"
 #endif
@@ -181,10 +176,6 @@ void AttachTabHelpers(content::WebContents* web_contents) {
   tor::TorTabHelper::MaybeCreateForWebContents(
       web_contents, web_contents->GetBrowserContext()->IsTor());
   tor::OnionLocationTabHelper::CreateForWebContents(web_contents);
-#endif
-
-#if BUILDFLAG(ENABLE_IPFS)
-  ipfs::IPFSTabHelper::MaybeCreateForWebContents(web_contents);
 #endif
 
   BraveNewsTabHelper::MaybeCreateForWebContents(web_contents);

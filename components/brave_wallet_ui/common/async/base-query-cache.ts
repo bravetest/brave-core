47a1c411c0ef07a1fcfd71c18d66c47e70721099
Vadym Struts <vstruts@brave.com>
2024-06-03 11:18:22 +0200

    fixed according recomendations in the PR
    Signed-off-by: Vadym Struts <vstruts@brave.com>

--- components/brave_wallet_ui/common/async/base-query-cache.ts
+++ components/brave_wallet_ui/common/async/base-query-cache.ts
@@ -308,7 +308,7 @@ export class BaseQueryCache {
   // responsible in providing correct logo.
   /** only caches ipfs translations since saving to a registry would require a
    * long identifier */
-  getTokenLogo = async (token: BraveWallet.BlockchainToken) => {
+  getTokenLogo = (token: BraveWallet.BlockchainToken) => {
     if (isNativeAsset(token)) {
       return makeNativeAssetLogo(token.symbol, token.chainId)
     }
@@ -321,8 +321,7 @@ export class BaseQueryCache {
       // nothing to change
       return token.logo
     }
-
-    return `chrome://erc-token-images/${token.logo}`
+    return token.logo.startsWith('ipfs://') ? '' : `chrome://erc-token-images/${token.logo}`
   }
 
   getEnabledCoinTypes = async () => {
@@ -520,7 +519,7 @@ async function fetchAssetsForNetwork({
     tokens,
     10,
     async (token: BraveWallet.BlockchainToken) => {
-      const tokenLogo = await cache.getTokenLogo(token)
+      const tokenLogo = cache.getTokenLogo(token)
       const updatedToken = addLogoToToken(token, tokenLogo)
       return addChainIdToToken(updatedToken, network.chainId)
     }

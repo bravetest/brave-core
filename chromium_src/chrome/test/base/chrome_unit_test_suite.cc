b15273e04067b0cf5b92a620aedb31fe2e103df4
mkarolin <max@brave.com>
2024-06-13 15:42:34 -0400

    fixup! Hook up Brave resource bundle to Chromium unit tests suite.

--- chromium_src/chrome/test/base/chrome_unit_test_suite.cc
+++ chromium_src/chrome/test/base/chrome_unit_test_suite.cc
@@ -8,13 +8,13 @@
 #include "brave/common/brave_content_client.h"
 #include "brave/common/resource_bundle_helper.h"
 #include "brave/test/base/testing_brave_browser_process.h"
-#include "chrome/browser/profiles/profile_shortcut_manager.h"
+#include "ui/base/resource/resource_bundle.h"
 
-#define DisableForUnitTests \
-  DisableForUnitTests();    \
-  brave::InitializeResourceBundle
+#define InitSharedInstanceWithLocale(...)    \
+  InitSharedInstanceWithLocale(__VA_ARGS__); \
+  brave::InitializeResourceBundle();
 
 #define ChromeContentClient BraveContentClient
 #include "src/chrome/test/base/chrome_unit_test_suite.cc"
 #undef ChromeContentClient
-#undef DisableForUnitTests
+#undef InitSharedInstanceWithLocale

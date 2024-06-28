713c4f7df2e13d148bcde49e50bf2c478965c8a0
Vadym Struts <vstruts@brave.com>
2024-06-28 14:37:31 +0200

    removed uneeded code
    Signed-off-by: Vadym Struts <vstruts@brave.com>

--- components/brave_wallet_ui/common/async/__mocks__/bridge.ts
+++ components/brave_wallet_ui/common/async/__mocks__/bridge.ts
@@ -1157,9 +1157,6 @@ export class MockedWalletApiProxy {
   braveWalletIpfsService: Partial<
     InstanceType<typeof BraveWallet.IpfsServiceInterface>
   > = {
-    extractIPFSUrlFromGatewayLikeUrl: async function (url: string) {
-      return { ipfsUrl: url }
-    },
     translateToGatewayURL: async function (url: string) {
       return {
         translatedUrl: url
